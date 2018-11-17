023a9e3d394d52a49f98fdb7c15d330a32523bfd
Teodor Sava <savateodor@yahoo.co.uk>
2018-11-17 21:34:04 +0100

    Add login and register

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const userRoutes = require("./api/routes/user");
+
+mongoose.connect(
+  "mongodb://localhost:27017/trip_planner",
+  {
+    useNewUrlParser: true
+  },
+  err => {
+    if (err) {
+      console.log(err);
+    }
+  }
+);
+
+mongoose.Promise = global.Promise;
+
+app.use(morgan("dev"));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+// route handler
+app.use("/user", userRoutes);
+
+// route error handler
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+// server error handler
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+module.exports = app;
