161638361369edc07e42350e05c78684939ddfde
Teodor Sava <savateodor@yahoo.co.uk>
2018-12-10 23:29:46 +0100

    add migration for places with tags based on the country. change place model to get more data from the google api

--- api/controllers/migrations.js
+++ api/controllers/migrations.js
@@ -6,10 +6,12 @@ const path = require('path');
 
 const appDir = path.dirname(require.main.filename);
 
+const config = require('../../config/config');
+//models
 const Place = require('../models/place');
 const City = require('../models/city');
 const Country = require('../models/country.js');
-
+const Tag = require('../models/tag');
 
 exports.populateCountries = (req, res, next) => {
     const countriesURL = `https://restcountries.eu/rest/v2/region/${req.params.region}`;
@@ -119,6 +121,105 @@ exports.populateCities = (req, res, next) => {
                 message: "No country found with that name"
             })
         });
+}
+
+exports.populatePlaces = (req, res, next) => {
+    // get the lat and long of the city in a country
+    const country = Country.findOne({
+            name: req.params.countryName
+        })
+        .exec()
+        .then(country => {
+            City.find({
+                    country: country._id
+                })
+                .exec()
+                .then(cities => {
+                    if (cities.length > 0) {
+                        let responseBody = [];
+                        cities.forEach(city => {
+                            const radius = 1500;
+                            const googlePlacesURL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' +
+                                city.coordinates.lat + ',' + city.coordinates.lng +
+                                '&radius=' + radius +
+                                '&type=restaurant' +
+                                '&key=' + config.google.API_KEY
+                            // make google api call for place
+
+                            request.get(googlePlacesURL, {
+                                json: true
+                            }, (err, response, body) => {
+                                if (body.status === 'OK') {
+                                    const places = body.results;
+                                    const path_to_error_file = `${appDir}/cron_reports/places_report_${moment().format('DD-MM-YYYY')}.txt`;
+                                    // for each place get the tags first then save the place information with the related tags information
+                                    let numberOfPlacesMigrated = 0;
+                                    places.forEach(async (placeAPI) => {
+                                        let tags;
+                                        tags = await Promise.all(placeAPI.types.map(async (tagAPI) => {
+                                            // save the tags in array to push them to the place
+                                            // search for tags in the db if not found save the tag
+                                            const tag = await Tag.findOne({
+                                                    name: tagAPI
+                                                })
+                                                .exec()
+                                                .then(tagDB => {
+                                                    return tagDB;
+                                                })
+                                                .then(tag => {
+                                                    if (!tag) {
+                                                        return tag.save()
+                                                    }
+                                                    return tag;
+                                                })
+                                                .then(tagToBePushed => tagToBePushed._id)
+                                                .catch((err) => console.log(err));
+                                            return tag._id;
+                                        }));
+
+                                        await Place.findOne({
+                                                name: placeAPI.name
+                                            }).exec()
+                                            .then(place => {
+                                                if (!place) {
+                                                    place = new Place({
+                                                        name: placeAPI.name,
+                                                        coordinates: {
+                                                            lat: placeAPI.geometry.location.lat,
+                                                            lng: placeAPI.geometry.location.lng
+                                                        },
+                                                        city: city._id,
+                                                        rating: placeAPI.rating,
+                                                        price_level: placeAPI.price_level,
+                                                        tags: tags.length > 0 ? tags : []
+                                                    });
+                                                    place.save();
+                                                    console.log(place);
+                                                }
+                                            })
+                                            .then(() => numberOfPlacesMigrated++)
+                                            .catch(err => writeToFile(path_to_error_file,
+                                                `place: ${place.name} name: ${city.name} error : ${err.message}`));
+
+                                    });
+
+                                    res.status(200).json({
+                                        message: `Number of places migrated ${numberOfPlacesMigrated}/${places.length}`
+                                    })
+                                }
+                            })
+                        })
+                    }
+                })
+                .catch(err => {
+                    res.status(404).json({
+                        message: "No city has been found for this country"
+                    })
+                });
+        })
+        .catch(err => res.status(404).json({
+            message: 'No country found with this name'
+        }));
 }
 
 function writeToFile(path, data) {
--- api/routes/migrations.js
+++ api/routes/migrations.js
@@ -5,5 +5,6 @@ const MigrationController = require('../controllers/migrations');
 
 router.get('/countries/:region', MigrationController.populateCountries);
 router.get('/cities/:countryName', MigrationController.populateCities);
+router.get('/places/:countryName', MigrationController.populatePlaces);
 
 module.exports = router;
